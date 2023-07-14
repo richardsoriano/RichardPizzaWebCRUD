81b0d0a8aa3036f15ab1c6fe0339f2d15f48bb10
richardsoriano <soriano.richard2020@gmail.com>
2023-07-14 16:09:46 -0700

    Add project files.

---
+++ RichardPizzaWebCRUD/Data/RichardPizzaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RichardPizzaWebCRUD.Models;
+
+namespace RichardPizzaWebCRUD.Data
+{
+    public class RichardPizzaContext : DbContext
+    {
+        public RichardPizzaContext() { }
+        public RichardPizzaContext(DbContextOptions<RichardPizzaContext> options)
+            : base(options) { }
+        public virtual DbSet<Product> Products { get; set; } = null!;
+    }
+}
---
+++ RichardPizzaWebCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RichardPizzaWebCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
